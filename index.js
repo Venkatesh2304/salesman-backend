88cd94384f89d63e66fe432d0c26aba55c40bea2
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2024-10-08 01:50:19 +0530

    s

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => { console.log('MongoDB connected'); })
   .catch(err => console.error(err));
 
-app.post('/get', async (req, res) => {
+app.get('/', async (req, res) => {
     console.log(1)
     return res.status(200).json({ "a" : 1 });
 });
